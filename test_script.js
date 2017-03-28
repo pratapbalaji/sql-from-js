d34c5b23d3f20c0c2bb308e95a3011ec0141bd9d
Vinay Balaji <vinaybalaji@gmail.com>
2017-03-28 19:52:23 +0000

    Added test_script.js file

--- test_script.js
+++ test_script.js
@@ -19,12 +19,20 @@ function getFamousPersonByName(name, callback) {
     if (err) {
       return console.error("error running query", err);
     }
-    callback(result.rows);
+    callback(name, result.rows);
     client.end();
     });
   });
 }
 
-getFamousPersonByName(process.argv[2], (result) => {
-  console.log(result);
-});
+function printFamousPerson(name, result) {
+  console.log('Searching ...');
+  console.log('Found ' + result.length + ' by the name ' + name);
+  for (let i = 0; i < result.length; i++) {
+    let birthDate = new Date(result[i].birthdate);
+    let formattedDate = birthDate.getFullYear() + "-" + (birthDate.getMonth()+1) + "-" + birthDate.getDate();
+    console.log('- ' + result[i].id + ': ' + result[i].first_name + ' ' + result[i].last_name + ', born ' + formattedDate + birthDate);
+  }
+}
+
+getFamousPersonByName(process.argv[2], printFamousPerson);
