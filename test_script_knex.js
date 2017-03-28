2f3ca2bff05d853c5b3fd78a678d1d08bf6e74da
Vinay Balaji <vinaybalaji@gmail.com>
2017-03-28 21:23:51 +0000

    Added test script files using Knex

--- test_script_knex.js
+++ test_script_knex.js
@@ -1,28 +1,27 @@
-const pg = require("pg");
 const settings = require("./settings"); // settings.json
 
-const client = new pg.Client({
-  user     : settings.user,
-  password : settings.password,
-  database : settings.database,
-  host     : settings.hostname,
-  port     : settings.port,
-  ssl      : settings.ssl
+const knex = require('knex')({
+  client   : 'pg',
+  connection: {
+    user     : settings.user,
+    password : settings.password,
+    database : settings.database,
+    host     : settings.hostname,
+    port     : settings.port,
+    ssl      : settings.ssl
+  }
 });
 
 function getFamousPersonByName(name, callback) {
-  client.connect((err) => {
-  if (err) {
-    return console.error("Connection Error", err);
-  }
-  client.query("SELECT * FROM famous_people WHERE first_name = $1 OR last_name = $1", [name], (err, result) => {
-    if (err) {
-      return console.error("error running query", err);
-    }
-    callback(name, result.rows);
-    client.end();
-    });
+  knex.select('id', 'first_name', 'last_name', 'birthdate').from('famous_people')
+  .where('first_name', '=', name)
+  .orWhere('last_name', '=', name)
+  .asCallback(function(err, rows) {
+    if (err) return console.error(err);
+    callback(name, rows);
+    knex.destroy();
   });
+
 }
 
 function printFamousPerson(name, result) {
